dd219fde969dc2b1834429f92aede59e4505bdad
Camuoiindasky <140451090+Camuoiindasky@users.noreply.github.com>
2023-08-04 01:03:54 -0500

    Update createNewGradeFile.py

--- Main Code/createNewGradeFile.py
+++ Main Code/createNewGradeFile.py
@@ -1,5 +1,15 @@
 import csv
+import os
 
-with open('grades.csv', 'w', newline='') as f0:
-    w0 = csv.writer(f0)
-    w0.writerow(['First Name','Last Name','Grade','Exam 1','Exam 2','Exam 3'])
+# Create New File
+
+stp = True
+while stp == True:
+    step2 = input('Want to create a new file? a/d/n ')
+    if step2 == 'y':
+        with open('grades.csv', 'w', newline='') as f0:
+            w0 = csv.writer(f0)
+            w0.writerow(['First Name','Last Name','Grade','Exam 1','Exam 2','Exam 3'])
+    elif step2 == 'n':
+        print()
+        stp = False
